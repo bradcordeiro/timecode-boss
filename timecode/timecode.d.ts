21429f434a0606b8c5e8c59f28db9be7e63c1a75
Brad Cordeiro <brad.cordeiro@gmail.com>
2021-01-11 22:59:12 -0800

    add types for typescript users

---
+++ timecode/timecode.d.ts
@@ -0,0 +1,42 @@
+export = Timecode;
+
+interface TimecodeLikeObject {
+  hours?: number;
+  minutes?: number;
+  seconds?: number;
+  frames?: number;
+  frameRate?: number;
+}
+
+declare class Timecode {
+  hours: number;
+  minutes: number;
+  seconds: number;
+  frames: number;
+  frameRate: number;
+
+  constructor(timecode: TimecodeLikeObject | number | string, frameRate?: number)
+
+  private set(input: TimecodeLikeObject | number | string) : this
+  private setFieldsFromFrameCount(input: number) : Timecode
+  private setFieldsFromString(input: string) : Timecode
+  private setFieldsFromObject(input: TimecodeLikeObject) : Timecode 
+  private setFieldsFromDate(date: Date) : this 
+  private separator() : string 
+  private framesInHoursField() : number 
+  private framesInMinutesField() : number
+  private framesInSecondsField() : number
+
+  toString() : string 
+  toObject() : TimecodeLikeObject 
+  setHours(hours: number) : this 
+  setMinutes(minutes: number) : this 
+  setSeconds(seconds: number) : this 
+  setFrames(frames : number) : this 
+  frameCount() : number 
+  isDropFrame() : boolean 
+  add(addend : TimecodeLikeObject | number | string) : Timecode 
+  subtract(subtrahend : TimecodeLikeObject | number | string) : Timecode 
+  pulldown(frameRate: number) : Timecode 
+  pullup(frameRate: number) : Timecode 
+}
